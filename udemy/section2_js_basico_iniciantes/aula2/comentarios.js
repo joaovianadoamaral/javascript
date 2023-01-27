a97ec822a5cd855e26dfc7c2f9fdb3943bf98e79
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-01-27 16:03:57 -0300

    Introdução aos comentários

---
+++ udemy/section2_js_basico_iniciantes/aula2/comentarios.js
@@ -0,0 +1,10 @@
+console.log('Olá mundo!');// Aqui tem outro comentário
+// console.log('Linha 1');
+
+/*
+Veja que nenhum dos comandos é executado 
+console.log('Linha 2');
+console.log('Linha 3');
+*/ 
+
+console.log('Linha 4');
