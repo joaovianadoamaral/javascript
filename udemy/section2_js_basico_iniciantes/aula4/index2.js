d4b2ad1e2ffd9ab503596285a802d1d43740fd4a
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-01-27 16:58:27 -0300

    introdução a variáveis em js

---
+++ udemy/section2_js_basico_iniciantes/aula4/index2.js
@@ -0,0 +1,4 @@
+// é possivel declarar uma variavel sem usar ela
+let nome;
+console.log(nome);
+//aparecerá undefined, 'indefinido'
---
+++ udemy/section2_js_basico_iniciantes/aula4/index3.js
@@ -0,0 +1,10 @@
+//declarando e inicializando uma variavel
+
+//inicializando uma variavel
+let nome;
+// variavel iniciada
+nome = 'João';
+console.log(nome);
+//mudando o valor da variavel
+nome = 'Otavio';
+console.log(nome)
