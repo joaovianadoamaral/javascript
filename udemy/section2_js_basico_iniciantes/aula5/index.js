99ec9d8a42cc6c0e3de21b35bef31480321b82a2
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-01-27 17:38:20 -0300

    introdução as constantes

---
+++ udemy/section2_js_basico_iniciantes/aula5/index.js
@@ -0,0 +1,12 @@
+const primeiroNumero = '5'
+const segundoNumero = 10;
+
+// + - * /
+const resultadoNum1xNum2 = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultadoNum1xNum2 * 2
+let resultadoTriplicado = resultadoNum1xNum2 * 3
+resultadoTriplicado = resultadoTriplicado + 5;
+
+//apesar de que as constantes não podem ser alteradas, 
+//podemos passar o valor delas para outras constantes
+console.log(typeof(resultadoNum1xNum2))
