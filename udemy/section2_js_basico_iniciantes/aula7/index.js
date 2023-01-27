d43a0c9b0e5d5cb59b460ba7f015a3c2ebc1e614
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-01-27 18:23:05 -0300

    ECMAscript, diferença entre var e let

---
+++ udemy/section2_js_basico_iniciantes/aula7/index.js
@@ -0,0 +1,10 @@
+let nome = 'joao';
+// não é possivel fazer     let nome    de novo
+var sobrenome = 'viana';
+var sobrenome = 'amaral';
+// com var é possivel, não recomendado
+
+//criando variaveis globais- não faça isso, pode atrapalhar o sistema
+pi = 3.141592
+
+console.log(nome, sobrenome);
