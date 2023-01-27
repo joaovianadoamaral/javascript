dc4a9a25e85820a5356d7ae9c2148edae5dad170
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-01-27 15:49:06 -0300

    aula 1 de js, introdução + prática

---
+++ udemy/section2_js_basico_iniciantes/aula1/exercicio/index.js
@@ -0,0 +1,6 @@
+// isso é um comentário em js
+// escreva na tela:
+// Meu nome é "NOME". Estou aprendendo JavaScript às x horas da manhã
+// x representa um número mesmo
+console.log('Meu nome é "João Vitor". Estou aprendendo JavaScript às', 15,'da tarde');
+// se colocar o 15 dentro das aspas ele também vai ser uma string 
