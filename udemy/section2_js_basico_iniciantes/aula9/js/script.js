92a328acd2a10d7224a60348032e32f0e50b895c
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-01-29 15:38:50 -0300

    diferença entre função e método, o objeto pai 'window', os métodos alert, confirm e prompt e os valores que eles retornam. uso de variaveis com funções

---
+++ udemy/section2_js_basico_iniciantes/aula9/js/script.js
@@ -0,0 +1,11 @@
+// peça dois números no prompt
+// mostre o resultado da soma usando alert
+let num1 = window.prompt('Digite um número:');
+let num2 = window.prompt('Digite outro número: ');
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+const somaNum1Num2 = num1 + num2;
+
+alert(`O resultado da sua conta foi: ${somaNum1Num2}`);
