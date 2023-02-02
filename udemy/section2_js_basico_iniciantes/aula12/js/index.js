aad376c27c2d65bffc15dbc5fa1ddf334bc4e5b0
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-02-01 21:59:06 -0300

    Number: conversão para string( pode ser usado para outras bases também), imprecisão do tipo float, converta para Number(com duas casas), Number.isNaN(variavel) se a variavel não é um número

---
+++ udemy/section2_js_basico_iniciantes/aula12/js/index.js
@@ -0,0 +1,37 @@
+let num1 = 1;
+let num2 = 2.5;
+let num3 = 10.53254316;
+
+let num1String = num1.toString();
+let num2String = num2.toString();
+//concatenação
+console.log(num1 + num2);
+console.log(num1 + num2String);
+//concatenação
+console.log(num1String + num2String);
+console.log(num1String + num2String);
+
+console.log(typeof num1); //número
+console.log(typeof num1String);//string
+
+// método toString para transformar um number em uma string();
+// se passarmos um número para esse método ele irá converter para a base decimal do número
+    // ex: let num1 = 30;
+    // let num1Hexa = num1.toString(16);
+    //console.log(num1Hexa) --> 1e
+// método toFixed(número de casas decimais)
+console.log('um número grande arredondado para duas casas decimais:')
+console.log('Número grande: ', num3);
+console.log('Número fixado: ', num3.toFixed(2));
+
+let temp = num1 * 'ola';
+// essa é uma conta falsa
+let contaVerdadeira = Number.isNaN(temp);
+// temp não é um número? true / false ->> false
+console.log(contaVerdadeira);
+
+// padrão IEEE 754-2008
+// os números com ponto flutuante são imprecisos.
+// * podemos transformar para inteiro
+// * melhor forma: Number(num1.toFixed(2))
+    // assim perde a imprecisão
